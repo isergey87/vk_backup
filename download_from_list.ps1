fe5f383099a036527b4cf7cc01b47f16e7bd0be9
isergey87 <isergey87@users.noreply.github.com>
2016-04-04 17:22:44 +0300

    Update download_from_list.ps1

--- download_from_list.ps1
+++ download_from_list.ps1
@@ -3,8 +3,9 @@ $lines = Get-Content -Encoding UTF8 vk_music.txt | Where {$_ -notmatch '^\s+$'}
 
 foreach ($line in $lines){
     $url = $line.Split('?')[0];		
-    $s = $line.Split(',')[1];
-    $title = $s.substring(4);
+    $ind = $line.IndexOf(',')+5;
+    $title = $line.substring($ind) -replace "[`"\\/?|<>:*]", " ";
+    $title = $title.replace("  "," ");
     $path = $dest, $title -join "\";
     $tempPath = $dest, "downloading.temp" -join "\";
     Write-Output "Downloading ($title) .....";
@@ -13,5 +14,5 @@ foreach ($line in $lines){
 # for windows 7
 #    (new-object System.Net.WebClient).DownloadFile($url,$path);
 
-Move-Item -literalpath $tempPath -destination $path;
+    Move-Item -literalPath $tempPath -destination "$path";
 }
