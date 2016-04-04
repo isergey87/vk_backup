b1637a73e31915fb1d0b08be19fd9d81f26c2181
isergey87 <isergey87@users.noreply.github.com>
2016-04-04 16:26:56 +0300

    Create download_from_list.ps1

---
+++ download_from_list.ps1
@@ -0,0 +1,12 @@
+$dest = "C:\temp"
+$lines = Get-Content -Encoding UTF8 vk_music.txt | Where {$_ -notmatch '^\s+$'}  
+
+foreach ($line in $lines){
+    $url = $line.Split('?')[0];		
+    $s = $line.Split(',')[1];
+    $title = $s.substring(4);
+    $path = $dest, $title -join "\";	
+    Write-Output "Downloading ($title) .....";
+    Invoke-WebRequest $url -OutFile $path;
+#    (new-object System.Net.WebClient).DownloadFile($url,$path);
+}
